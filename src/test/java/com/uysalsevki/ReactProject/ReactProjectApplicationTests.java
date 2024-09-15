eb32f275a0787253de5c30a05b4a88667de1ad33
sevki uysal <uysalsevki.9@gmail.com>
2024-09-15 15:33:38 +0300

    Spring boot initialization

---
+++ src/test/java/com/uysalsevki/ReactProject/ReactProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uysalsevki.ReactProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
